1bd235b9624f772cbacb770d1b7b6dee37dfa316
Lucas Maritan Whitaker <lucasmaritanlab@gmail.com>
2021-08-20 10:27:17 -0300

    1º - BASE
    ROTAS /
    Controllers
    Action Link
    Css 1ª Página
    Config ASP.NET p/ Forms Authentication
    Pág Login

---
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControleEstoque.Web.Models;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+
+
+
+            return View(login);
+        }
+    }
+}
